61f2023914316a960e013f32a1badb3053e9d111
Hp <ahmedelzanaty001@gmail.com>
2024-12-03 20:06:29 +0200

    home screen + bottom navigation bar

--- app/src/main/java/com/example/todoapp/Activitys/HomeActivity.kt
+++ app/src/main/java/com/example/todoapp/Activitys/HomeActivity.kt
@@ -5,17 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.todoapp.Fragments.ListFragment
+import com.example.todoapp.Fragments.SettingFragment
 import com.example.todoapp.R
+import com.example.todoapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomAppBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.list_ic -> pushfragment(ListFragment())
+                R.id.setting_ic -> pushfragment(SettingFragment())
+            }
+            return@setOnItemSelectedListener true
         }
+        binding.bottomAppBar.selectedItemId = R.id.list_ic
+
+
     }
+    fun pushfragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.fragments.id, fragment).commit()
+    }
+
 }
