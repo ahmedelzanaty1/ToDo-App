e562936662af7ab2f1088f4c0e743ce45c5cbc6e
Hp <ahmedelzanaty001@gmail.com>
2024-12-08 18:12:51 +0200

    is task completed

--- app/src/main/java/com/example/todoapp/adapters/TasksAdapter.kt
+++ app/src/main/java/com/example/todoapp/adapters/TasksAdapter.kt
@@ -3,13 +3,17 @@ package com.example.todoapp.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.Model.Task
+import com.example.todoapp.R
 import com.example.todoapp.databinding.TaskRecyclerviewBinding
+import com.example.todoapp.pack_interface.OnClickListener
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class TasksAdapter(var tasks: List<Task>) : RecyclerView.Adapter<TasksAdapter.Taskviewholder>() {
+    var onClickListener: OnClickListener? = null
 
     class Taskviewholder(val binding: TaskRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(task: Task) {
@@ -32,6 +36,20 @@ class TasksAdapter(var tasks: List<Task>) : RecyclerView.Adapter<TasksAdapter.Ta
     override fun onBindViewHolder(holder: Taskviewholder, position: Int) {
         val task = tasks[position]
         holder.bind(task)
+        holder.binding.img.setOnClickListener {
+            onClickListener?.onclick(position, task)
+            holder.binding.tv.setTextColor(
+                holder.itemView.context.resources.getColor(R.color.green, null)
+            )
+            holder.binding.img.setImageResource(R.drawable.check_ic)
+            holder.binding.img.setColorFilter(
+                holder.itemView.context.resources.getColor(R.color.green, null)
+            )
+            holder.binding.view1.setBackgroundColor(
+                holder.itemView.context.resources.getColor(R.color.green, null)
+            )
+
+        }
     }
     fun updateData(newTasks: List<Task>) {
         tasks = newTasks
